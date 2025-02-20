f44e5589d075dde2dfe562a5e49e70d36053dceb
Michal <145454583+MichalKuma@users.noreply.github.com>
2025-02-20 18:24:31 +0100

    Delete UPCOMING PORT directory

--- UPCOMING PORT/script.js
+++
@@ -1,9 +0,0 @@
-// Toggle menu on mobile
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-});
-
